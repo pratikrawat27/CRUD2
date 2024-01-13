b07f0a23895d2f5d623347485cf0fa2c5ec41993
pratikrawat27 <pratikrawat27@gamil.com>
2024-01-13 14:53:53 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+function addData(){
+    let name = document.getElementById("fname").value;
+    let email = document.getElementById("email").value;
+    let mnumber = document.getElementById("mobileNumber").value;
+
+    const table = document.getElementById('dataTable').value;
+
+    const addRow = table.insertRow(table.rows.length);
+
+    let cell1 = newRow.insertCell(0);
+    let cell2 = newRow.insertCell(1);
+    let cell3 = newRow.insertCell(2);
+    let cell4 = newRow.insertCell(3);
+
+    
+}
