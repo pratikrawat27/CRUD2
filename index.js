247cd1dd58f71ad24898bf04073d9af4608d9ba1
pratikrawat <pratikrawat27@gmail.com>
2024-01-14 13:01:13 +0530

    added row and made input blank after submit

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
-function addData(){
-    let name = document.getElementById("fname").value;
-    let email = document.getElementById("email").value;
-    let mnumber = document.getElementById("mobileNumber").value;
+let idCount = 1;
 
-    const table = document.getElementById('dataTable').value;
+function addData(){
+    let name = document.getElementById('fname').value;
+    let email = document.getElementById('email').value;
+    let mnumber = document.getElementById('mobileNumber').value;
+
+    const table = document.getElementById('dataTable');
 
     const addRow = table.insertRow(table.rows.length);
 
-    let cell1 = newRow.insertCell(0);
-    let cell2 = newRow.insertCell(1);
-    let cell3 = newRow.insertCell(2);
-    let cell4 = newRow.insertCell(3);
-    let cell5 = newRow.insertCell(4);
+    let cell1 = addRow.insertCell(0);
+    let cell2 = addRow.insertCell(1);
+    let cell3 = addRow.insertCell(2);
+    let cell4 = addRow.insertCell(3);
 
-    idCount = 1;
     cell1.innerHTML = idCount++;
     cell2.innerHTML = name;
     cell3.innerHTML = mnumber;
     cell4.innerHTML = email;
-    cell5.innerHTML = `<button onClick="" ></button>`
 
-    
+    name = document.getElementById('fname').value = '';
+    email = document.getElementById("email").value = '';
+    mnumber = document.getElementById('mobileNumber').value = '';
 }
